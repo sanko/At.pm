b967487045e66885ef0d809322acade9f1ca29c6
Sanko Robinson <sanko@cpan.org>
2024-01-06 15:55:26 -0500

    Start getting ready for another push to PAUSE

--- eg/text_post.pl
+++ eg/text_post.pl
@@ -2,18 +2,16 @@ use strict;
 use warnings;
 #
 use lib '../lib', 'lib', '../blib/arch', '../blib/lib', 'blib/arch', 'blib/lib', '../../', '.';
-use At;    # Or use At::Bluesky
+use At;                              # Or use At::Bluesky
+use At::Lexicon::app::bsky::feed;    # Or use At::Bluesky
 use Config::Tiny;
 use Time::Piece;
 $|++;
 #
 my $config = Config::Tiny->read('bluesky.conf');
-my $at     = At->new( host => 'https://bsky.social' );
-$at->server->createSession( %{ $config->{_} } );
-$at->repo->createRecord(
-    collection => 'app.bsky.feed.post',
-    record     => { '$type' => 'app.bsky.feed.post', text => 'Hello world! I posted this via the API.', createdAt => gmtime->datetime . 'Z' }
-);
+my $at     = At->new( host => 'https://bsky.social', %{ $config->{_} } );
+$at->repo_createRecord( $at->did, 'app.bsky.feed.post',
+    { '$type' => 'app.bsky.feed.post', text => 'Hello world! I posted this via the API.', createdAt => gmtime->datetime . 'Z' } );
 __END__
 Create a config file named bluesky.conf with contents that look like this:
 
