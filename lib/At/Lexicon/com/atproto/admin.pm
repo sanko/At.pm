341dd9c454a4fb2f094652a3f176021c17c907c6
Sanko Robinson <sanko@cpan.org>
2024-01-22 20:49:25 -0500

    Checking in changes prior to tagging of version 0.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b0b324c..88078bb 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension At.pm
     
     {{$NEXT}}
     
    +0.08 2024-01-23T01:48:21Z
    +
         - Resume expired sessions with $at->server_refreshSession( ... )
     
     0.07 2024-01-20T22:54:55Z

--- lib/At/Lexicon/com/atproto/admin.pm
+++ lib/At/Lexicon/com/atproto/admin.pm
@@ -1,4 +1,4 @@
-package At::Lexicon::com::atproto::admin 0.07 {
+package At::Lexicon::com::atproto::admin 0.08 {
 
     #~ https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/admin/defs.json
     use v5.38;
--- lib/At/Lexicon/com/atproto/moderation.pm
+++ lib/At/Lexicon/com/atproto/moderation.pm
@@ -1,4 +1,4 @@
-package At::Lexicon::com::atproto::moderation 0.07 {
+package At::Lexicon::com::atproto::moderation 0.08 {
 
     #~ https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/moderation/defs.json
     use v5.38;
